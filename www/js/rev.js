b0a8832551cc919a6885126d2e00a544ee412f09
AracelyGE <aracelygarcia13@hotmail.com>
2018-08-07 19:29:38 -0500

    Initial commit

---
+++ www/js/rev.js
@@ -0,0 +1,22 @@
+$('#sig1').click(function(){
+    
+						var co= document.querySelector('input[name="a'+data[i].id+'"]:checked').value;
+				
+						alert(co);
+						});
+					}
+/if(arr.length==0){
+						//arr.push(co);}else{arr.push(co);}
+
+
+						//if(arr[x] == data[x].ress){ alert('correcto pregunta '+  data[x].preg + ' es = ' + arr[x]);}
+						//alert(arr);
+
+
+					//daniiiii
+
+					/*if (document.getElementByTagName("'a"+data[x].id+"'").checked){
+							dani = document.querySelector("'a"+data[x].id+"'").value;
+							aler(dani);
+						}*/
+
